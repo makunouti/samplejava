b27409f4d156c1709a16681e7a4c2511dfd56897
makunouti <vandaru3@gmail.com>
2023-12-17 10:46:04 +0900

    "練習"

--- src/App.java
+++ src/App.java
@@ -1,5 +1,22 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+class Main {
+  public static void main(String[] args) {
+    // int型の変数numberを定義してください
+    int number;
+    
+    // 変数numberに3を代入してください
+    number = 3;
+    
+    // 変数numberを出力してください
+    System.out.println(number);
+
+    // String型の変数nameを定義してください
+    String name;
+    
+    // 変数nameに"Wanko"を代入してください
+    name = "Wanko";
+    
+    // 変数nameを出力してください
+    System.out.println(name);
+    
+  }
 }
